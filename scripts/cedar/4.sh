a544168947083b444b5a28394c916660d385b0e5
RajGhugare19 <rajghugare.vnit@gmail.com>
2023-01-21 10:25:08 +0530

    3 seeds are enough rn

--- scripts/cedar/4.sh
+++ scripts/cedar/4.sh
@@ -5,7 +5,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=4G
 #SBATCH --gres=gpu:v100l:1
-#SBATCH --array=1-5
+#SBATCH --array=1-3
 
 rsync -a $HOME/projects/def-gberseth/$USER/RL4Chem/ $SLURM_TMPDIR/RL4Chem --exclude=env_rl4chem
 
