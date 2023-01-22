6f29027ecdedc7111ebbd3a5dad5cf91160cab68
RajGhugare19 <rajghugare.vnit@gmail.com>
2023-01-22 16:18:42 +0530

    which len to solve

--- scripts/cedar/3.sh
+++ scripts/cedar/3.sh
@@ -7,10 +7,10 @@
 #SBATCH --gres=gpu:v100l:1
 #SBATCH --array=1-5
 
-array=(10 20 30 40)
+array=(-1 10 20 30 40 48)
 
 rsync -a $HOME/projects/def-gberseth/$USER/RL4Chem/ $SLURM_TMPDIR/RL4Chem --exclude=env_rl4chem
 
 cd $SLURM_TMPDIR/RL4Chem
 
-python train.py max_selfie_length=${array[1]} wandb_log=True seed=3 wandb_run_name=max_len${array[1]}_ent0.1_seed3
+python train.py max_selfie_length=${array[SLURM_ARRAY_TASK_ID]} wandb_log=True seed=3 wandb_run_name=max_len${array[SLURM_ARRAY_TASK_ID]}_ent0.1_seed3
