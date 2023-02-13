ec87eb76fa55f56f05bb370c274601ff6d1ab674
RajGhugare19 <rajghugare.vnit@gmail.com>
2023-02-13 15:06:34 +0530

    testing whether agent find optimality or not

--- env.py
+++ env.py
@@ -161,49 +161,23 @@ class docking_env(object):
         return reward_batch, info
 
 if __name__ == '__main__':
-    import time
-    start = time.time()
-    target = 'fa7'
-    docking_config = dict()
+    from dataclasses import dataclass
 
-    if target == 'fa7':
-        box_center = (10.131, 41.879, 32.097)
-        box_size = (20.673, 20.198, 21.362)
-        docking_config['receptor_file'] = 'ReLeaSE_Vina/docking/fa7/receptor.pdbqt'
-    elif target == 'parp1':
-        box_center = (26.413, 11.282, 27.238)
-        box_size = (18.521, 17.479, 19.995)
-        docking_config['receptor_file'] = 'ReLeaSE_Vina/docking/parp1/receptor.pdbqt'
-    elif target == '5ht1b':
-        box_center = (-26.602, 5.277, 17.898)
-        box_size = (22.5, 22.5, 22.5)
-        docking_config['receptor_file'] = 'ReLeaSE_Vina/docking/5ht1b/receptor.pdbqt'
+    @dataclass
+    class args():
+        target= 'fa7'
+        selfies_enc_type= 'one_hot'
+        max_selfie_length= 9
+        vina_program= 'qvina2'
+        temp_dir= 'tmp'
+        exhaustiveness= 1
+        num_sub_proc= 12
+        num_cpu_dock= 1
+        num_modes= 10
+        timeout_gen3d= 30
+        timeout_dock= 100
 
-    box_parameter = (box_center, box_size)
-    docking_config['vina_program'] = 'qvina2'
-    docking_config['box_parameter'] = box_parameter
-    docking_config['temp_dir'] = 'tmp'
-    docking_config['exhaustiveness'] = 1
-    docking_config['num_sub_proc'] = 12
-    docking_config['num_cpu_dock'] = 12
-    docking_config['num_modes'] = 10 
-    docking_config['timeout_gen3d'] = 30
-    docking_config['timeout_dock'] = 100 
-
-    predictor = DockingVina(docking_config)
-
-    selfie_alphabet_set = sf.get_semantic_robust_alphabet()
-    smiles_benzene = "c1ccccc1"
-    selfies_benzene = sf.encoder(smiles_benzene)
-    selfie_alphabet_set.add(selfies_benzene)
-    selfie_alphabets = list(selfie_alphabet_set)
-
-    smiles_list = []
-    for i in range(10):
-        selfie_str=''.join(np.random.choice(selfie_alphabets, 40))
-        decoded_smile_str = sf.decoder(selfie_str)
-        smiles_list.append(decoded_smile_str)
-    
-    a_list = predictor.predict(smiles_list)
-    print(a_list)
-    print(time.time()-start)
+    env = docking_env(args)
+    possible_states = []
+    actions =  env.action_space
+    print(env.reset())
