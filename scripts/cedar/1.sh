e284f9c6a51b7e55bfdbf5287dfba49597b58211
RajGhugare19 <rajghugare.vnit@gmail.com>
2023-02-16 19:03:35 +0530

    trials script for docker

--- scripts/cedar/1.sh
+++ scripts/cedar/1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --account=rrg-gberseth
-#SBATCH --time=4:00:00
+#SBATCH --time=00:20:00
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=4G
 #SBATCH --gres=gpu:v100l:1
@@ -9,7 +9,5 @@
 rsync -a $HOME/projects/def-gberseth/$USER/RL4Chem/ $SLURM_TMPDIR/RL4Chem --exclude=env_rl4chem
 echo "moved code to slurm tmpdir"
 
-singularity exec --nv --home $SLURM_TMPDIR --env WANDB_API_KEY="7da29c1c6b185d3ab2d67e399f738f0f0831abfc" --env REQUESTS_CA_BUNDLE="/usr/local/lib/python3.9/site-packages/certifi/cacert.pem" $SCRATCH/rl4chem.sif bash -c "source activate rl4chem && cd RL4Chem &&\
+singularity exec --nv --home $SLURM_TMPDIR --env WANDB_API_KEY="7da29c1c6b185d3ab2d67e399f738f0f0831abfc", REQUESTS_CA_BUNDLE="/usr/local/envs/rl4chem/lib/python3.11/site-packages/certifi/cacert.pem", HYDRA_FULL_ERROR=1 $SCRATCH/rl4chem.sif bash -c "source activate rl4chem && cd RL4Chem &&\
 python train.py wandb_log=True seed=1 num_sub_proc=24" 
-
-# array=(-1 10 20 30 40 48) SBATCH --array=1-5
