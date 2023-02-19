ecb61c0ff688a553e55e29721e995e7478c12144
RajGhugare19 <rajghugare.vnit@gmail.com>
2023-02-19 22:43:59 +0530

    adding scripts for larger lengths

--- scripts/cedar/3.sh
+++ scripts/cedar/3.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 #SBATCH --account=rrg-gberseth
-#SBATCH --time=5:00:00
-#SBATCH --cpus-per-task=4
+#SBATCH --time=02:00:00
+#SBATCH --cpus-per-task=24
 #SBATCH --mem=4G
 #SBATCH --gres=gpu:v100l:1
-#SBATCH --array=1-5
+#SBATCH --array=1-3
 
-array=(-1 10 20 30 40 48)
+array=(-1 "fa7" "parp1" "5ht1b")
 
 rsync -a $HOME/projects/def-gberseth/$USER/RL4Chem/ $SLURM_TMPDIR/RL4Chem --exclude=env_rl4chem
+echo "moved code to slurm tmpdir"
 
-cd $SLURM_TMPDIR/RL4Chem
-
-python train.py max_selfie_length=${array[SLURM_ARRAY_TASK_ID]} wandb_log=True seed=3 wandb_run_name=max_len${array[SLURM_ARRAY_TASK_ID]}_ent0.1_seed3
+singularity exec --nv --home $SLURM_TMPDIR --env WANDB_API_KEY="7da29c1c6b185d3ab2d67e399f738f0f0831abfc",REQUESTS_CA_BUNDLE="/usr/local/envs/rl4chem/lib/python3.11/site-packages/certifi/cacert.pem",HYDRA_FULL_ERROR=1 $SCRATCH/rl4chem.sif bash -c "source activate rl4chem && cd RL4Chem &&\
+python train.py target=${array[SLURM_ARRAY_TASK_ID]} max_selfie_length=25 wandb_log=True wandb_run_name=L_max_len_25 seed=1 num_sub_proc=24" 
