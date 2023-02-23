3eefc6fde9da769be01f3e9958b1f9cebbd7a49b
RajGhugare19 <rajghugare.vnit@gmail.com>
2023-02-23 11:40:20 +0530

    adding mila scripts

--- regression/scripts/mila/1.sh
+++ regression/scripts/mila/1.sh
@@ -2,12 +2,12 @@
 
 #SBATCH -t 00:30:00
 #SBATCH -c 4
-#SBATCH --partition=unkillable
+#SBATCH --partition=main
 #SBATCH --mem=4G
 #SBATCH --gres=gpu:1
 #SBATCH --array=1-3
 
-array=(-1 "ESR2" "F2" "KIT" "PARP1" "PGR")
+array=(-1 "0.5" "10" "100")
 
 module --quiet load anaconda/3
 conda activate rl4chem
@@ -16,8 +16,5 @@ echo "activated conda environment"
 rsync -a $HOME/RL4Chem/ $SLURM_TMPDIR/RL4Chem
 echo "moved code to slurm tmpdir"
 
-
 cd $SLURM_TMPDIR/RL4Chem/regression
-python regression.py target=${array[SLURM_ARRAY_TASK_ID]} seed=1 wandb_log=True wandb_run_name=adam_charnn_1&
-python regression.py target=${array[SLURM_ARRAY_TASK_ID]} seed=2 wandb_log=True wandb_run_name=adam_charnn_2&
-python regression.py target=${array[SLURM_ARRAY_TASK_ID]} seed=3 wandb_log=True wandb_run_name=adam_charnn_3
+python regression.py max_grad_norm=${array[SLURM_ARRAY_TASK_ID]} seed=1 wandb_log=True wandb_run_name=${array[SLURM_ARRAY_TASK_ID]}_charnn_1
