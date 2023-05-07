c48a9fa45ad5791df41c106a3a2869f18b1b84c2
RajGhugare19 <rajghugare.vnit@gmail.com>
2023-05-07 09:51:23 +0530

    other pretraining datasets are not implemented for RNNs right now

--- train_reinforce_rnn_agent.py
+++ train_reinforce_rnn_agent.py
@@ -41,6 +41,8 @@ class reinforce_optimizer(BaseOptimizer):
             if cfg.max_len > max_dataset_len:
                 cfg.max_len = max_dataset_len
                 print('*** Changing the maximum length of sampled molecules because it was set to be greater than the maximum length seen during training ***')
+        else:
+            raise NotImplementedError
         
         #get data
         if cfg.rep == 'smiles':
