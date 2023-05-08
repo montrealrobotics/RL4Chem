78f81c11143f83b7e31ad68880342b4ce1444dcb
RajGhugare19 <rajghugare.vnit@gmail.com>
2023-05-08 15:33:58 +0530

    testing early exit

--- scripts/beluga/2.sh
+++ scripts/beluga/2.sh
@@ -15,9 +15,9 @@ rsync -a $HOME/projects/def-gberseth/$USER/RL4Chem/ $SLURM_TMPDIR/RL4Chem --excl
 cd $SLURM_TMPDIR/RL4Chem
 
 wandb offline
-python train_reinforce_fc_agent.py target=drd2 seed=1 wandb_log=True wandb_dir='.' wandb_run_name='sdsd_'${s} #&
+python train_reinforce_fc_agent.py target=drd2 seed=1 wandb_log=True wandb_dir='.' wandb_run_name='sdsd_'${s} &
 
-# python train_reinforce_fc_agent.py target=drd2 seed=1 rep=selfies wandb_log=True wandb_run_name='sdsd_'${s}
+python train_reinforce_fc_agent.py target=drd2 seed=1 rep=selfies wandb_log=True wandb_run_name='sdsd_'${s}
 
 a="local_exp"
 mkdir -p $HOME/projects/def-gberseth/$USER/RL4Chem/$a
