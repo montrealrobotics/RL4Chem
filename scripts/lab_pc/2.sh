cf6723858aa995a903dd9a1cf6fbf598ff76f780
RajGhugare19 <rajghugare.vnit@gmail.com>
2023-05-08 17:31:57 +0530

    adding lab scripts

--- scripts/lab_pc/2.sh
+++ scripts/lab_pc/2.sh
@@ -2,15 +2,15 @@ targets=('troglitazone_rediscovery' 'sitagliptin_mpo' 'median2')
 
 for t in ${targets[@]}; 
 do
-    python ../../train_reinforce_rnn_agent.py oracle=$t seed=1 rep=selfies wandb_log=True wandb_run_name='std_selfies_1'
+    python ../../train_reinforce_rnn_agent.py target=$t seed=1 rep=selfies wandb_log=True wandb_run_name='std_selfies_1'
 done
 
 for t in ${targets[@]}; 
 do
-    python ../../train_reinforce_rnn_agent.py oracle=$t seed=2 rep=selfies wandb_log=True wandb_run_name='std_selfies_1'
+    python ../../train_reinforce_rnn_agent.py target=$t seed=2 rep=selfies wandb_log=True wandb_run_name='std_selfies_1'
 done
 
 for t in ${targets[@]}; 
 do
-    python ../../train_reinforce_rnn_agent.py oracle=$t seed=3 rep=selfies wandb_log=True wandb_run_name='std_selfies_1'
+    python ../../train_reinforce_rnn_agent.py target=$t seed=3 rep=selfies wandb_log=True wandb_run_name='std_selfies_1'
 done
