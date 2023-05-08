4ffc13bbf437292e362dc49ce3bc349fe850a7ca
RajGhugare19 <rajghugare.vnit@gmail.com>
2023-05-08 12:26:49 +0530

    forced to use wandb

--- scripts/beluga/2.sh
+++ scripts/beluga/2.sh
@@ -6,7 +6,6 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-node=v100:1
 
-module load httpproxy
 echo "activating env"
 source $HOME/projects/def-gberseth/$USER/RL4Chem/env_chem/bin/activate
 
@@ -15,8 +14,12 @@ rsync -a $HOME/projects/def-gberseth/$USER/RL4Chem/ $SLURM_TMPDIR/RL4Chem --excl
 
 cd $SLURM_TMPDIR/RL4Chem
 
-python train_reinforce_fc_agent.py target=drd2 seed=1 wandb_log=True wandb_run_name='sdsd_'${s} #&
+wandb offline
+python train_reinforce_fc_agent.py target=drd2 seed=1 wandb_log=True wandb_dir='.' wandb_run_name='sdsd_'${s} #&
 
 # python train_reinforce_fc_agent.py target=drd2 seed=1 rep=selfies wandb_log=True wandb_run_name='sdsd_'${s}
 
+a="local_exp"
+mkdir -p $HOME/projects/def-gberseth/$USER/RL4Chem/$a
+cp -r $SLURM_TMPDIR/RL4Chem/wandb $HOME/projects/def-gberseth/$USER/RL4Chem/$a
 echo "done"
