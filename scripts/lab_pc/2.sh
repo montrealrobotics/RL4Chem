fa17f673fd4bfbbc096630a729d8678498831b0c
RajGhugare19 <rajghugare.vnit@gmail.com>
2023-05-14 13:44:19 +0530

    lets do only one seed now

--- scripts/lab_pc/2.sh
+++ scripts/lab_pc/2.sh
@@ -5,12 +5,12 @@ do
     python ../../train_reinforce_trans_agent.py target=$t seed=1 task=augmented_docking num_sub_proc=24 dataset=chembl wandb_log=True wandb_run_name='reinforce_chembl_1'
 done
 
-for t in ${targets[@]}; 
-do
-    python ../../train_reinforce_trans_agent.py target=$t seed=2 task=augmented_docking num_sub_proc=24 dataset=chembl wandb_log=True wandb_run_name='reinforce_chembl_2'
-done
+# for t in ${targets[@]}; 
+# do
+#     python ../../train_reinforce_trans_agent.py target=$t seed=2 task=augmented_docking num_sub_proc=24 dataset=chembl wandb_log=True wandb_run_name='reinforce_chembl_2'
+# done
 
-for t in ${targets[@]}; 
-do
-    python ../../train_reinforce_trans_agent.py target=$t seed=3 task=augmented_docking num_sub_proc=24 dataset=chembl wandb_log=True wandb_run_name='reinforce_chembl_3'
-done
+# for t in ${targets[@]}; 
+# do
+#     python ../../train_reinforce_trans_agent.py target=$t seed=3 task=augmented_docking num_sub_proc=24 dataset=chembl wandb_log=True wandb_run_name='reinforce_chembl_3'
+# done
