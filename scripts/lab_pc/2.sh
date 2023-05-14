7f6171b850bf9a23bd9415377e27a93294f82600
RajGhugare19 <rajghugare.vnit@gmail.com>
2023-05-14 13:25:09 +0530

    adding trans agents for augmented docking

--- scripts/lab_pc/2.sh
+++ scripts/lab_pc/2.sh
@@ -1,16 +1,16 @@
-targets=('troglitazone_rediscovery' 'sitagliptin_mpo' 'median2')
+targets=('fa7')
 
 for t in ${targets[@]}; 
 do
-    python ../../train_reinforce_rnn_agent.py target=$t seed=1 rep=selfies penalty=diff wandb_log=True wandb_run_name='dif_selfies_1'
+    python ../../train_reinforce_trans_agent.py target=$t seed=1 task=augmented_docking num_sub_proc=24 dataset=chembl wandb_log=True wandb_run_name='reinforce_chembl_1'
 done
 
 for t in ${targets[@]}; 
 do
-    python ../../train_reinforce_rnn_agent.py target=$t seed=2 rep=selfies penalty=diff wandb_log=True wandb_run_name='dif_selfies_2'
+    python ../../train_reinforce_trans_agent.py target=$t seed=2 task=augmented_docking num_sub_proc=24 dataset=chembl wandb_log=True wandb_run_name='reinforce_chembl_2'
 done
 
 for t in ${targets[@]}; 
 do
-    python ../../train_reinforce_rnn_agent.py target=$t seed=3 rep=selfies penalty=diff wandb_log=True wandb_run_name='dif_selfies_3'
+    python ../../train_reinforce_trans_agent.py target=$t seed=3 task=augmented_docking num_sub_proc=24 dataset=chembl wandb_log=True wandb_run_name='reinforce_chembl_3'
 done
---
+++ scripts/lab_pc/4.sh
@@ -0,0 +1,16 @@
+targets=('fa7')
+
+for t in ${targets[@]}; 
+do
+    python ../../train_reinforce_trans_agent.py target=$t seed=1 task=augmented_docking num_sub_proc=24 dataset=zinc1m wandb_log=True wandb_run_name='reinforce_zinc1m_1'
+done
+
+for t in ${targets[@]}; 
+do
+    python ../../train_reinforce_trans_agent.py target=$t seed=2 task=augmented_docking num_sub_proc=24 dataset=zinc1m wandb_log=True wandb_run_name='reinforce_zinc1m_2'
+done
+
+for t in ${targets[@]}; 
+do
+    python ../../train_reinforce_trans_agent.py target=$t seed=3 task=augmented_docking num_sub_proc=24 dataset=zinc1m wandb_log=True wandb_run_name='reinforce_zinc1m_3'
+done
