583720cc32be96c273e0c8ef0663f6bcbf97f0f1
RajGhugare19 <rajghugare.vnit@gmail.com>
2023-05-16 22:16:31 +0530

    adding reinvent

--- scripts/cedar/1.sh
+++ scripts/cedar/1.sh
@@ -30,4 +30,4 @@ rsync -a $HOME/projects/def-gberseth/$USER/RL4Chem/ $SLURM_TMPDIR/RL4Chem --excl
 
 cd $SLURM_TMPDIR/RL4Chem
 
-python train_reinforce_rnn_agent.py target=${t} seed=${s} wandb_log=True wandb_run_name='diff_reinforce_zinc250k_char_trans_selfies_'${s}
+python train_reinventog.py target=${t} seed=${s} wandb_log=True wandb_run_name='reiventog_'${s}
---
+++ train_reinventog.py
@@ -0,0 +1,494 @@
+import os
+import time
+import wandb
+import hydra
+import torch
+import random
+import numpy as np
+from tdc import Oracle
+from omegaconf import DictConfig
+from optimizer import BaseOptimizer
+import numpy as np
+import re
+from rdkit import Chem
+import torch
+from torch.utils.data import Dataset
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+path_here = os.path.dirname(os.path.realpath(__file__))
+
+def set_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+
+def get_params(model):
+    return (p for p in model.parameters() if p.requires_grad)
+
+def Variable(tensor):
+    """Wrapper for torch.autograd.Variable that also accepts
+       numpy arrays directly and automatically assigns it to
+       the GPU. Be aware in case some operations are better
+       left to the CPU."""
+    if isinstance(tensor, np.ndarray):
+        tensor = torch.from_numpy(tensor).float()
+    if torch.cuda.is_available():
+        return torch.autograd.Variable(tensor).cuda()
+    return torch.autograd.Variable(tensor)
+
+def seq_to_smiles(seqs, voc):
+    """Takes an output sequence from the RNN and returns the
+       corresponding SMILES."""
+    smiles = []
+    for seq in seqs.cpu().numpy():
+        smiles.append(voc.decode(seq))
+    return smiles
+
+def unique(arr):
+    # Finds unique rows in arr and return their indices
+    arr = arr.cpu().numpy()
+    arr_ = np.ascontiguousarray(arr).view(np.dtype((np.void, arr.dtype.itemsize * arr.shape[1])))
+    _, idxs = np.unique(arr_, return_index=True)
+    if torch.cuda.is_available():
+        return torch.LongTensor(np.sort(idxs)).cuda()
+    return torch.LongTensor(np.sort(idxs))
+
+class MultiGRU(nn.Module):
+    """ Implements a three layer GRU cell including an embedding layer
+       and an output linear layer back to the size of the vocabulary"""
+    def __init__(self, voc_size):
+        super(MultiGRU, self).__init__()
+        self.embedding = nn.Embedding(voc_size, 128)
+        self.gru_1 = nn.GRUCell(128, 512)
+        self.gru_2 = nn.GRUCell(512, 512)
+        self.gru_3 = nn.GRUCell(512, 512)
+        self.linear = nn.Linear(512, voc_size)
+
+    def forward(self, x, h):
+        x = self.embedding(x)
+        h_out = Variable(torch.zeros(h.size()))
+        x = h_out[0] = self.gru_1(x, h[0])
+        x = h_out[1] = self.gru_2(x, h[1])
+        x = h_out[2] = self.gru_3(x, h[2])
+        x = self.linear(x)
+        return x, h_out
+
+    def init_h(self, batch_size):
+        # Initial cell state is zero
+        return Variable(torch.zeros(3, batch_size, 512))
+
+class RNN():
+    """Implements the Prior and Agent RNN. Needs a Vocabulary instance in
+    order to determine size of the vocabulary and index of the END token"""
+    def __init__(self, voc):
+        self.rnn = MultiGRU(voc.vocab_size)
+        print('need to wait several minutes')
+        if torch.cuda.is_available():
+            self.rnn.cuda()
+        self.voc = voc
+
+    def likelihood(self, target):
+        """
+            Retrieves the likelihood of a given sequence
+            Args:
+                target: (batch_size * sequence_lenght) A batch of sequences
+            Outputs:
+                log_probs : (batch_size) Log likelihood for each example*
+                entropy: (batch_size) The entropies for the sequences. Not
+                                      currently used.
+        """
+        batch_size, seq_length = target.size()
+        start_token = Variable(torch.zeros(batch_size, 1).long())
+        start_token[:] = self.voc.vocab['GO']
+        x = torch.cat((start_token, target[:, :-1]), 1)
+        h = self.rnn.init_h(batch_size)
+
+        log_probs = Variable(torch.zeros(batch_size).float())
+        entropy = Variable(torch.zeros(batch_size))
+        for step in range(seq_length):
+            logits, h = self.rnn(x[:, step], h)
+            log_prob = F.log_softmax(logits)
+            prob = F.softmax(logits)
+            log_probs += NLLLoss(log_prob, target[:, step])
+            entropy += -torch.sum((log_prob * prob), 1)
+        return log_probs, entropy
+
+    def sample(self, batch_size, max_length=140):
+        """
+            Sample a batch of sequences
+            Args:
+                batch_size : Number of sequences to sample 
+                max_length:  Maximum length of the sequences
+            Outputs:
+            seqs: (batch_size, seq_length) The sampled sequences.
+            log_probs : (batch_size) Log likelihood for each sequence.
+            entropy: (batch_size) The entropies for the sequences. Not
+                                    currently used.
+        """
+        start_token = Variable(torch.zeros(batch_size).long())
+        start_token[:] = self.voc.vocab['GO']
+        h = self.rnn.init_h(batch_size)
+        x = start_token
+
+        sequences = []
+        log_probs = Variable(torch.zeros(batch_size))
+        finished = torch.zeros(batch_size).byte()
+        entropy = Variable(torch.zeros(batch_size))
+        if torch.cuda.is_available():
+            finished = finished.cuda()
+
+        # for step in range(max_length):
+        #     logits, h = self.rnn(x, h)
+        #     prob = F.softmax(logits)
+        #     log_prob = F.log_softmax(logits)
+        #     x = torch.multinomial(prob).view(-1)
+        #     sequences.append(x.view(-1, 1))
+        #     log_probs +=  NLLLoss(log_prob, x)
+        #     entropy += -torch.sum((log_prob * prob), 1)
+
+        for step in range(max_length):
+            logits, h = self.rnn(x, h)
+            # print('logits shape', logits.shape) ##### [128, 109]
+            prob = F.softmax(logits, dim = 1)
+            log_prob = F.log_softmax(logits, dim = 1)
+            x = torch.multinomial(prob, num_samples=1).view(-1)
+            sequences.append(x.view(-1, 1))
+            log_probs +=  NLLLoss(log_prob, x)
+            entropy += -torch.sum((log_prob * prob), 1)
+
+            x = Variable(x.data)
+            EOS_sampled = (x == self.voc.vocab['EOS']).data
+            finished = torch.ge(finished + EOS_sampled, 1)
+            if torch.prod(finished) == 1: break
+
+        sequences = torch.cat(sequences, 1)
+        return sequences.data, log_probs, entropy
+
+def NLLLoss(inputs, targets):
+    """
+        Custom Negative Log Likelihood loss that returns loss per example,
+        rather than for the entire batch.
+        Args:
+            inputs : (batch_size, num_classes) *Log probabilities of each class*
+            targets: (batch_size) *Target class index*
+        Outputs:
+            loss : (batch_size) *Loss for each example*
+    """
+
+    if torch.cuda.is_available():
+        target_expanded = torch.zeros(inputs.size()).cuda()
+    else:
+        target_expanded = torch.zeros(inputs.size())
+
+    target_expanded.scatter_(1, targets.contiguous().view(-1, 1).data, 1.0)
+    loss = Variable(target_expanded) * inputs
+    loss = torch.sum(loss, 1)
+    return loss
+
+
+class Vocabulary(object):
+    """A class for handling encoding/decoding from SMILES to an array of indices"""
+    def __init__(self, init_from_file=None, max_length=140):
+        self.special_tokens = ['EOS', 'GO']
+        self.additional_chars = set()
+        self.chars = self.special_tokens
+        self.vocab_size = len(self.chars)
+        self.vocab = dict(zip(self.chars, range(len(self.chars))))
+        self.reversed_vocab = {v: k for k, v in self.vocab.items()}
+        self.max_length = max_length
+        if init_from_file: self.init_from_file(init_from_file)
+
+    def encode(self, char_list):
+        """Takes a list of characters (eg '[NH]') and encodes to array of indices"""
+        smiles_matrix = np.zeros(len(char_list), dtype=np.float32)
+        for i, char in enumerate(char_list):
+            smiles_matrix[i] = self.vocab[char]
+        return smiles_matrix
+
+    def decode(self, matrix):
+        """Takes an array of indices and returns the corresponding SMILES"""
+        chars = []
+        for i in matrix:
+            if i == self.vocab['EOS']: break
+            chars.append(self.reversed_vocab[i])
+        smiles = "".join(chars)
+        smiles = smiles.replace("L", "Cl").replace("R", "Br")
+        return smiles
+
+    def tokenize(self, smiles):
+        """Takes a SMILES and return a list of characters/tokens"""
+        regex = '(\[[^\[\]]{1,6}\])'
+        smiles = replace_halogen(smiles)
+        char_list = re.split(regex, smiles)
+        tokenized = []
+        for char in char_list:
+            if char.startswith('['):
+                tokenized.append(char)
+            else:
+                chars = [unit for unit in char]
+                [tokenized.append(unit) for unit in chars]
+        tokenized.append('EOS')
+        return tokenized
+
+    def add_characters(self, chars):
+        """Adds characters to the vocabulary"""
+        for char in chars:
+            self.additional_chars.add(char)
+        char_list = list(self.additional_chars)
+        char_list.sort()
+        self.chars = char_list + self.special_tokens
+        self.vocab_size = len(self.chars)
+        self.vocab = dict(zip(self.chars, range(len(self.chars))))
+        self.reversed_vocab = {v: k for k, v in self.vocab.items()}
+
+    def init_from_file(self, file):
+        """Takes a file containing \n separated characters to initialize the vocabulary"""
+        with open(file, 'r') as f:
+            chars = f.read().split()
+        self.add_characters(chars)
+
+    def __len__(self):
+        return len(self.chars)
+
+    def __str__(self):
+        return "Vocabulary containing {} tokens: {}".format(len(self), self.chars)
+
+class MolData(Dataset):
+    """Custom PyTorch Dataset that takes a file containing SMILES.
+        Args:
+                fname : path to a file containing \n separated SMILES.
+                voc   : a Vocabulary instance
+        Returns:
+                A custom PyTorch dataset for training the Prior.
+    """
+    def __init__(self, fname, voc):
+        self.voc = voc
+        self.smiles = []
+        with open(fname, 'r') as f:
+            for line in f:
+                self.smiles.append(line.split()[0])
+
+    def __getitem__(self, i):
+        mol = self.smiles[i]
+        tokenized = self.voc.tokenize(mol)
+        encoded = self.voc.encode(tokenized)
+        return Variable(encoded)
+
+    def __len__(self):
+        return len(self.smiles)
+
+    def __str__(self):
+        return "Dataset containing {} structures.".format(len(self))
+
+    @classmethod
+    def collate_fn(cls, arr):
+        """Function to take a list of encoded sequences and turn them into a batch"""
+        max_length = max([seq.size(0) for seq in arr])
+        collated_arr = Variable(torch.zeros(len(arr), max_length))
+        for i, seq in enumerate(arr):
+            collated_arr[i, :seq.size(0)] = seq
+        return collated_arr
+
+class Experience(object):
+    """Class for prioritized experience replay that remembers the highest scored sequences
+       seen and samples from them with probabilities relative to their scores."""
+    def __init__(self, voc, max_size=100):
+        self.memory = []
+        self.max_size = max_size
+        self.voc = voc
+
+    def add_experience(self, experience):
+        """Experience should be a list of (smiles, score, prior likelihood) tuples"""
+        self.memory.extend(experience)
+        if len(self.memory)>self.max_size:
+            # Remove duplicates
+            idxs, smiles = [], []
+            for i, exp in enumerate(self.memory):
+                if exp[0] not in smiles:
+                    idxs.append(i)
+                    smiles.append(exp[0])
+            self.memory = [self.memory[idx] for idx in idxs]
+            # Retain highest scores
+            self.memory.sort(key = lambda x: x[1], reverse=True)
+            self.memory = self.memory[:self.max_size]
+            # print("\nBest score in memory: {:.2f}".format(self.memory[0][1]))
+
+    def sample(self, n):
+        """Sample a batch size n of experience"""
+        if len(self.memory)<n:
+            raise IndexError('Size of memory ({}) is less than requested sample ({})'.format(len(self), n))
+        else:
+            scores = [x[1]+1e-10 for x in self.memory]
+            sample = np.random.choice(len(self), size=n, replace=False, p=scores/np.sum(scores))
+            sample = [self.memory[i] for i in sample]
+            smiles = [x[0] for x in sample]
+            scores = [x[1] for x in sample]
+            prior_likelihood = [x[2] for x in sample]
+        tokenized = [self.voc.tokenize(smile) for smile in smiles]
+        encoded = [Variable(self.voc.encode(tokenized_i)) for tokenized_i in tokenized]
+        encoded = MolData.collate_fn(encoded)
+        return encoded, np.array(scores), np.array(prior_likelihood)
+
+    def initiate_from_file(self, fname, scoring_function, Prior):
+        """Adds experience from a file with SMILES
+           Needs a scoring function and an RNN to score the sequences.
+           Using this feature means that the learning can be very biased
+           and is typically advised against."""
+        with open(fname, 'r') as f:
+            smiles = []
+            for line in f:
+                smile = line.split()[0]
+                if Chem.MolFromSmiles(smile):
+                    smiles.append(smile)
+        scores = scoring_function(smiles)
+        tokenized = [self.voc.tokenize(smile) for smile in smiles]
+        encoded = [Variable(self.voc.encode(tokenized_i)) for tokenized_i in tokenized]
+        encoded = MolData.collate_fn(encoded)
+        prior_likelihood, _ = Prior.likelihood(encoded.long())
+        prior_likelihood = prior_likelihood.data.cpu().numpy()
+        new_experience = zip(smiles, scores, prior_likelihood)
+        self.add_experience(new_experience)
+
+    def print_memory(self, path):
+        """Prints the memory."""
+        print("\n" + "*" * 80 + "\n")
+        print("         Best recorded SMILES: \n")
+        print("Score     Prior log P     SMILES\n")
+        with open(path, 'w') as f:
+            f.write("SMILES Score PriorLogP\n")
+            for i, exp in enumerate(self.memory[:100]):
+                if i < 50:
+                    print("{:4.2f}   {:6.2f}        {}".format(exp[1], exp[2], exp[0]))
+                    f.write("{} {:4.2f} {:6.2f}\n".format(*exp))
+        print("\n" + "*" * 80 + "\n")
+
+    def __len__(self):
+        return len(self.memory)
+
+def replace_halogen(string):
+    """Regex to replace Br and Cl with single letters"""
+    br = re.compile('Br')
+    cl = re.compile('Cl')
+    string = br.sub('R', string)
+    string = cl.sub('L', string)
+
+    return string
+
+class reinventog_optimizer(BaseOptimizer):
+    def __init__(self, cfg=None):
+        super().__init__(cfg)
+        self.agent_name = cfg.agent_name
+    
+    def optimize(self, cfg):
+        # self.oracle.assign_evaluator(oracle)
+        device = torch.device(cfg.device)
+        set_seed(cfg.seed)
+
+        #get data
+        voc = Vocabulary(init_from_file=os.path.join(path_here, "Voc"))
+
+        Prior = RNN(voc)
+        Agent = RNN(voc)
+        restore_prior_from=os.path.join(path_here, 'Prior.ckpt')
+        restore_agent_from=restore_prior_from 
+        Prior.rnn.load_state_dict(torch.load(os.path.join(path_here,'Prior.ckpt')))
+        Agent.rnn.load_state_dict(torch.load(restore_agent_from))
+
+        # We dont need gradients with respect to Prior
+        for param in Prior.rnn.parameters():
+            param.requires_grad = False
+
+        optimizer = torch.optim.Adam(Agent.rnn.parameters(), lr=cfg.learning_rate)
+
+        experience = Experience(voc)
+
+        print("Model initialized, starting training...")
+
+        step = 0
+
+        while True:
+           
+            # Sample from Agent
+            seqs, agent_likelihood, entropy = Agent.sample(cfg.batch_size)
+
+            # Remove duplicates, ie only consider unique seqs
+            unique_idxs = unique(seqs)
+            seqs = seqs[unique_idxs]
+            agent_likelihood = agent_likelihood[unique_idxs]
+            entropy = entropy[unique_idxs]
+
+            # Get prior likelihood and score
+            prior_likelihood, _ = Prior.likelihood(seqs)
+            smiles = seq_to_smiles(seqs, voc)
+            score = np.array(self.predict(smiles))
+
+            if self.finish:
+                print('max oracle hit')
+                break 
+
+            # Calculate augmented likelihood
+            augmented_likelihood = prior_likelihood + cfg.sigma * torch.tensor(score, dtype=torch.float32, device=device)
+            loss = torch.pow((augmented_likelihood - agent_likelihood), 2)
+
+            # Experience Replay
+            if cfg.experience_replay and len(experience)>cfg.experience_replay:
+                exp_seqs, exp_score, exp_prior_likelihood = experience.sample(cfg.experience_replay)
+                exp_agent_likelihood, exp_entropy = Agent.likelihood(exp_seqs.long())
+                exp_augmented_likelihood = torch.tensor(exp_prior_likelihood + cfg.sigma * exp_score, dtype=torch.float32, device=device)
+                exp_loss = torch.pow((exp_augmented_likelihood - exp_agent_likelihood), 2)
+                loss = torch.cat((loss, exp_loss), 0)
+                agent_likelihood = torch.cat((agent_likelihood, exp_agent_likelihood), 0)
+
+            # Then add new experience
+            prior_likelihood = prior_likelihood.detach().cpu().numpy()
+            new_experience = zip(smiles, score, prior_likelihood)
+            experience.add_experience(new_experience)
+
+            # Calculate loss
+            loss = loss.mean()
+
+            # Add regularizer that penalizes high likelihood for the entire sequence
+            loss_p = - (1 / agent_likelihood).mean()
+            loss += 5 * 1e3 * loss_p
+
+            # Calculate gradients and make an update to the network weights
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # Convert to numpy arrays so that we can print them
+            augmented_likelihood = augmented_likelihood.detach().cpu().numpy()
+            agent_likelihood = agent_likelihood.detach().cpu().numpy()
+
+            if cfg.wandb_log and step % cfg.train_log_interval == 0:
+                metrics = dict()
+                metrics['augmented_likelihood'] = augmented_likelihood.mean()
+                metrics['agent_likelihood'] = agent_likelihood.mean()
+                metrics['loss'] = loss.item()
+                metrics['loss_p'] = loss_p.item()
+                metrics['max memory cuda'] = torch.cuda.max_memory_allocated(device)/ 1000000
+                wandb.log(metrics)
+
+            step += 1
+
+@hydra.main(config_path='cfgs', config_name='reinventog', version_base=None)
+def main(cfg: DictConfig):
+    hydra_cfg = hydra.core.hydra_config.HydraConfig.get()
+    
+    if cfg.wandb_log:
+        project_name = 'pmo_' + cfg.oracle
+        wandb_run = wandb.init(project=project_name, entity=cfg.wandb_entity, config=dict(cfg))
+        wandb.run.name = cfg.wandb_run_name
+    
+    set_seed(cfg.seed)
+    cfg.output_dir = hydra_cfg['runtime']['output_dir']
+    oracle = Oracle(name = cfg.target)
+    optimizer = reinventog_optimizer(cfg)
+    optimizer.optimize(cfg)
+
+if __name__ == '__main__':
+    main()
