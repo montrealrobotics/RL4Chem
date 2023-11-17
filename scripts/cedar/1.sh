2b47355885c0737ed2e91e0202acbf8e9c1da551
RajGhugare19 <rajghugare.vnit@gmail.com>
2023-11-16 19:05:50 -0500

    adding unfinished scriots

--- scripts/cedar/1.sh
+++ scripts/cedar/1.sh
@@ -5,15 +5,11 @@
 #SBATCH --mem=4G
 #SBATCH --cpus-per-task=4
 #SBATCH --gpus-per-node=1
-#SBATCH --array=1-23
+#SBATCH --array=1-11
 
 targets=('drd2' 'qed' 'jnk3' 'gsk3b' 'celecoxib_rediscovery'\
-        'troglitazone_rediscovery'\
-        'thiothixene_rediscovery' 'albuterol_similarity' 'mestranol_similarity'\
-        'isomers_c7h8n2o2' 'isomers_c9h10n2o2pf2cl' 'median1' 'median2' 'osimertinib_mpo'\
-        'fexofenadine_mpo' 'ranolazine_mpo' 'perindopril_mpo' 'amlodipine_mpo'\
-        'sitagliptin_mpo' 'zaleplon_mpo' 'valsartan_smarts' 'deco_hop' 'scaffold_hop')
-        
+        'troglitazone_rediscovery' 'thiothixene_rediscovery' 'albuterol_similarity' 'mestranol_similarity' 'isomers_c7h8n2o2' 'sitagliptin_mpo')        
+
 seeds=(1)
 
 t=${targets[$(((SLURM_ARRAY_TASK_ID-1) % 23))]}
